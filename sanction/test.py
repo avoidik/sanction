8f73c81e2e86a0e88dd12d28fb46b3cb3b347a87
avoidik <avoidik@gmail.com>
2024-03-28 16:47:01 +0000

    final touches, add gitlab support

--- sanction/test.py
+++ sanction/test.py
@@ -1,5 +1,5 @@
 # vim: set ts=4 sw=4 et:
-from io import BytesIO 
+from io import BytesIO
 from functools import wraps
 try:
     from urllib2 import addinfourl
@@ -17,14 +17,15 @@ def with_patched_client(data, code=200, headers=None):
         @wraps(fn)
         def inner(*args, **kwargs):
             with patch('sanction.urlopen') as mock_urlopen:
-                bdata = type(data) is basestring and data.encode() or data
+                bdata = isinstance(data, basestring) and data.encode() or data
                 sheaders = ''
                 if headers is not None:
-                    sheaders = '\r\n'.join(['{}: {}'.format(k, v) for k, v in
-                        headers.items()])
+                    sheaders = '\r\n'.join(['{}: {}'.format(k, v)
+                                            for k, v in headers.items()])
                 bheaders = (sheaders or '').encode()
 
-                mock_urlopen.return_value = addinfourl(BytesIO(bdata), 
+                mock_urlopen.return_value = addinfourl(
+                    BytesIO(bdata),
                     HTTPMessage(BytesIO(bheaders)), '', code=code)
                 fn(*args, **kwargs)
         return inner
